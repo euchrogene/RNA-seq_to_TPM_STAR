26335fe990b3ca4dde37994ae12ae99cf8863770
EuchroGene, LLC <165866904+euchrogene@users.noreply.github.com>
2026-01-27 14:14:05 -0500

    Update Install_RNA_seq_to_TPM_STAR.sh

--- Install_RNA_seq_to_TPM_STAR.sh
+++ Install_RNA_seq_to_TPM_STAR.sh
@@ -8,7 +8,7 @@ PIPELINE_ENTRY="$EXE_FILE => A pipeline to process RNA-seqs to get TPM, FPKM, an
 
 TARGET_BIN="/usr/bin"
 DATA_FILE="/usr/share/euchrogene_tools.txt"
-VIEWER_SCRIPT="$TARGET_BIN/eg-tools"
+VIEWER_SCRIPT="$TARGET_BIN/EG_tools"
 
 echo "Step 1: Downloading repository..."
 # Clean up any previous failed attempts first
